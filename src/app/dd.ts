17851e32f04f88d3694151f09122b39c800e2533
queonias <queones@gmail.com>
2023-03-25 16:36:32 -0300

    fix

--- src/app/dd.ts
+++ src/app/dd.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
 const routes: Routes = [];
 
 @NgModule({
-  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
